c9c7152338f3c9ff4029653c968fb8c9e5c907df
HIMANSHI NAITHANI <HIMANSHI NAITHANI@LAPTOP-TS5GLF1T>
2022-03-24 22:31:27 -0700

    added P2_Task02_keywords

---
+++ JavaLearning/src/Unit_01/P2_Task02_Keywords.java
@@ -0,0 +1,26 @@
+package Unit_01;
+
+public class P2_Task02_Keywords {
+
+	public static void main(String[] args) {
+		 
+		
+		int double = 10; // Syntax error on token "double", invalid VariableDeclaratorId
+		
+		static void int () // Syntax error on token "int", invalid AnnotationName
+		{
+			System.out.println( "hello world");
+		}
+        ABC int = new ABC(); //Syntax error on token "int", delete this token
+        
+		  
+		}
+	}
+class ABC 
+{
+	int P2()
+	{
+		System.out.println( "ABC");
+	}
+}
+
