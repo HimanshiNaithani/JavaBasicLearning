d72c3ef21becaf49e20c447a254e21b353e5f602
HIMANSHI NAITHANI <HIMANSHI NAITHANI@LAPTOP-TS5GLF1T>
2022-03-24 22:23:56 -0700

    Second commit

---
+++ JavaLearning/src/Unit_01/P2_Task01_ConstantsInJava.java
@@ -0,0 +1,23 @@
+package Unit_01;
+
+public class P2_Task01_ConstantsInJava {
+
+		 static final double pi = 3.14; // GLOBAL CONSTANT  
+
+		 public static void main(String[] args) {
+				
+				final int r = 10;
+				double area = pi*r*r;
+				
+				 System.out.println(area);
+				  
+				 System.out.println(pi);
+
+			}
+
+}
+
+
+	
+
+
